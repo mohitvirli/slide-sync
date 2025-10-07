619f27555e6e373e8e12608002086373d196d933
Mohit Virli <virlimohit@gmail.com>
2025-10-07 20:42:29 +0530

    Remove Nextjs logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
 'use client';
 
 import { generateRoomId, isValidRoomId, normalizeRoomId } from "@/lib/id";
-import { nanoid } from "nanoid";
-import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 
@@ -25,7 +23,6 @@ export default function Home() {
   return (
      <div className="min-h-screen font-mono flex flex-col items-center justify-center p-8">
       <div className="max-w-md w-full text-center">
-        <Image src="/next.svg" alt="Next.js" width={180} height={38} />
         <h1 className="text-3xl font-bold mt-6">Slides App</h1>
         <p className="mt-2 text-sm text-muted-foreground">Create a room and collaborate in real time.</p>
 
